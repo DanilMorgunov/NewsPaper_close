37c4ac6d2e9905f3d8fc607603140a07e61074a7
Name <daniil.morgunov@mail.ru>
2023-01-04 14:27:09 +0300

    initial commit

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostList(ListView):
+    # название модели
+    model = Post
+    # поле для сортировки - по дате создания ( '-' - от конца)
+    ordering = '-dateCreation'
+
+    template_name = 'news.html'
+
+    context_object_name = 'news'
+
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
